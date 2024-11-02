f49a810751fca53577e5fcdd3d6ce6b99c4ceafd
Actung10 <1140732361@qq.com>
2024-11-02 17:23:14 +0800

    Add files via upload

--- add/add.js
+++ add/add.js
@@ -5,22 +5,17 @@ import { getAccount, setAccount } from '../../utils/storage'
 Page({
   data: {
     type: 0,  // 0 支出，1 收入，2 不计入收支
-
     date: '',
-
     money: '',
-
+    account: 'wechat',
     typeObj: expendType[0],
     expendType,
     incomeType,
     otherType,
-
-    // 是否添加备注
     isAddRemark: false,
     remark: '',
-
-    isAdd: false,       // 添加请求是否结束, 用于入账按钮加载效果
-    isDisabled: false,  // 是否禁用入账按钮，防止多次触发
+    isAdd: false,
+    isDisabled: false,
   },
 
   // 切换头部收支类型
@@ -35,7 +30,7 @@ Page({
 
     this.setData({
       type,
-      typeObj: obj,
+      typeObj: obj
     })
   },
   
@@ -57,7 +52,7 @@ Page({
   switchTypeName (e) {
     const typeObj = e.detail
     this.setData({
-      typeObj,
+      typeObj
     })
   },
 
@@ -87,21 +82,74 @@ Page({
       return
     }
     
-    const { type, date,  typeObj, account, remark } = this.data
-
-    const dataToSave = {
-      type,
-      date,
-      typeObj,
-      account,
-      remark
-    };
-
+    const { type, date, typeObj, account, remark } = this.data
+    
     this.setData({
       isAdd: true,
       isDisabled: true
     })
-    wx.setStorageSync('accounts', dataToSave);
+    
+    // 存储到本地
+    this._saveToLocal(type, date, typeObj, account, remark, money).then(() => {
+      this.setData({
+        money: '',
+        remark: ''
+      })
+      wx.reLaunch({
+        url: '/pages/index/index'
+      })
+    }).catch(() => {
+      wx.showToast({
+        title: '存储失败',
+        icon: 'none'
+      })
+    }).finally(() => {
+      this.setData({
+        isAdd: false,
+        isDisabled: false
+      })
+    })
   },
 
+  _saveToLocal (type, date, typeObj, account, remark, money) {
+    return new Promise((resolve, reject) => {
+      // 获取当前存储的记账记录
+      const records = wx.getStorageSync('records') || [];
+      
+      // 创建新的记账记录
+      const newRecord = {
+        type,
+        date,
+        name: typeObj.type,
+        account,
+        remark,
+        money: parseFloat(money) * 100, // 将金额转换为分为单位
+      };
+      
+      // 将新记录添加到数组中
+      records.push(newRecord);
+      
+      // 存储更新后的记录到本地存储
+      wx.setStorageSync('records', records);
+      
+      // 更新账户余额
+      this._loadStorage(account, type, parseFloat(money) * 100);
+      
+      resolve();
+    });
+  },
+
+  _loadStorage (account, type, money) {
+    const accountObj = getAccount();
+
+    if (type === 0) {
+      accountObj[`${account}`] -= money;
+      if (accountObj[`${account}`] < 0) accountObj[`${account}`] = 0;
+    }
+    if (type === 1) accountObj[`${account}`] += money;
+
+    setAccount({
+      ...accountObj
+    });
+  }
 })
